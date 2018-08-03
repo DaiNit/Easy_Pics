3c0189a2faf5535c0d8fe10aeba8ba25911b4a99
DaiNit <41444108+DaiNit@users.noreply.github.com>
2018-08-03 10:53:13 -0500

    Update index.jsx

--- settings/index.jsx
+++ settings/index.jsx
@@ -5,7 +5,7 @@ function mySettings(props) {
   return (
     <Page>    
       <Section
-        title={<Text>Keep EasyPics App active while you upload pics. You may need to select pics several times.</Text>}>
+        title={<Text>Keep EasyPics App active while you upload pics. The picture you upload will be add as for Pic1. Please choose different pictures every time.</Text>}>
       </Section>
       <Section
         title="Picture Quality and Transfer speed">
@@ -20,65 +20,9 @@ function mySettings(props) {
         />
       </Section>
      <ImagePicker
-          title="Picture No.1"
-          label="Pick an image for Pic 1"
-          settingsKey="image1"
-          imageWidth={ screenWidth }
-          imageHeight={ screenHeight }
-        />
-        <ImagePicker
-         title="Picture No.2"
-          label="Pick an image for Pic 2"
-          settingsKey="image2"
-          imageWidth={ screenWidth }
-          imageHeight={ screenHeight }
-        />
-        <ImagePicker
-         title="Picture No.3"
-          label="Pick an image for Pic 3"
-          settingsKey="image3"
-          imageWidth={ screenWidth }
-          imageHeight={ screenHeight }
-        />
-        <ImagePicker
-         title="Picture No.4"
-          label="Pick an image for Pic 4"
-          settingsKey="image4"
-          imageWidth={ screenWidth }
-          imageHeight={ screenHeight }
-        />
-        <ImagePicker
-         title="Picture No.5"
-          label="Pick an image for Pic 5"
-          settingsKey="image5"
-          imageWidth={ screenWidth }
-          imageHeight={ screenHeight }
-        />
-        <ImagePicker
-         title="Picture No.6"
-          label="Pick an image for Pic 6"
-          settingsKey="image6"
-          imageWidth={ screenWidth }
-          imageHeight={ screenHeight }
-        />
-        <ImagePicker
-         title="Picture No.7"
-          label="Pick an image for Pic 7"
-          settingsKey="image7"
-          imageWidth={ screenWidth }
-          imageHeight={ screenHeight }
-        />
-        <ImagePicker
-         title="Picture No.8"
-          label="Pick an image for Pic 8"
-          settingsKey="image8"
-          imageWidth={ screenWidth }
-          imageHeight={ screenHeight }
-        />
-        <ImagePicker
-         title="Picture No.9"
-          label="Pick an image for Pic 9"
-          settingsKey="image9"
+          title="Picture upload"
+          label="Pick an image to upload"
+          settingsKey="image"
           imageWidth={ screenWidth }
           imageHeight={ screenHeight }
         />
